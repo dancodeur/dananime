a90271232f86134c517004db9469dd94cd7f4b79
ELENGA Dan David <elengadan@gmail.com>
2024-03-07 08:32:54 +0100

    initialisation : danamine streaming - projet web festival IUT Cergy-Pontoise Mars 2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import React from 'react';
+import { Button, CustomProvider, Container } from 'rsuite';
 import logo from './logo.svg';
+import 'rsuite/dist/rsuite.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CustomProvider theme="light">
+      <Container className="app">
+        <header className="app-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <Button href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </Button>
+        </header>
+      </Container>
+    </CustomProvider>
   );
 }
 
